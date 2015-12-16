1878c73f68002ede04581a1f45e9af96e226d755
Ben Elliott <benjam.elliott@gmail.com>
2015-12-15 17:23:57 -0700

    Change the Portrait Treatment For the About Page
    Have the users table refer to the portrait table too
    Make changes in the service and view to account for the change

--- php/rosterdata.php
+++ php/rosterdata.php
@@ -6,7 +6,16 @@ class RosterService extends SchemaGamesService
 {
     public function run()
     {
-        $sql = 'SELECT nickname, fullname, title, default_portrait FROM users';
+        $sql = <<<'SQL'
+SELECT
+	nickname,
+	fullname,
+	title,
+	ports.portrait_file
+FROM users
+	INNER JOIN portraits
+		ON users.default_portrait = portraits.portrait_id
+SQL;
         $resultSet = $this->query($sql);
         $this->render($resultSet);
     }
