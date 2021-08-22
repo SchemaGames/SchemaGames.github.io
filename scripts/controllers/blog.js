8810c2b3e69bc004b0e72f05be61ace01bdc7b8c
Ben Elliott <benjam.elliott@gmail.com>
2021-08-21 19:42:19 -0600

    Update Articles/Blog to Use Static JSON Sources

--- scripts/controllers/blog.js
+++ scripts/controllers/blog.js
@@ -24,11 +24,21 @@
 				$scope.articleslist[i].article_text = $scope.articleslist[i].article_text.replace(re_newline,"<br />");
 			}
 		};
+		$scope.nil = {};
+		$scope.takeWhile = function(f, [x = nil, ...xs]) {
+			return (x === nil || !f(x)) ? [] : [x, ...$scope.takeWhile(f, xs)];
+		};
+		$scope.dropWhile = function(f, [x = nil, ...xs]) {
+			return (x === nil) ? [] : 
+				(f(x) ? $scope.dropWhile(f, xs) : [x, ...xs]);
+		};
 		//AJAX requests
 		$scope.getArticles = function() {
-			$http.get('https://api.schemagames.com/article?limit='+$scope.articlesPerPage) 
+			//$http.get('https://api.schemagames.com/article?limit='+$scope.articlesPerPage)
+			$http.get('db/articles.json') 
 			.success(function (data) {
-				$scope.articleslist = data;
+				let sortedData = data.sort((a, b) => b.post_time - a.post_time).slice(0, $scope.articlesPerPage);
+				$scope.articleslist = sortedData;
 				//Set pagination
 				if(typeof $scope.articleslist !== 'undefined' && $scope.articleslist.length > 0) {
 					var topTime = parseFloat($scope.articleslist[0].post_time) + 1;
@@ -46,15 +56,17 @@
 		};
 		$scope.getArticlesOlder = function(posttime) {
 			var newestPostTime = parseFloat(posttime);
-			$http.get('https://api.schemagames.com/article?limit='+$scope.articlesPerPage+'&older='+newestPostTime)
+			$http.get('db/articles.json')
 			.success(function (data) {
-				$scope.articleslist = data;
+				let sortedData = data.sort((a, b) => b.post_time - a.post_time);
+				let takenData = $scope.dropWhile((x) => x.post_time >= newestPostTime, sortedData).slice(0, $scope.articlesPerPage);
+				$scope.articleslist = takenData;
 				//Set pagination
 				if($scope.articleslist.length > 0) {
 					var topTime = parseFloat($scope.articleslist[0].post_time) + 1;
 					var botTime = parseFloat($scope.articleslist[$scope.articleslist.length - 1].post_time) - 1;
-					$scope.setNewerLink(topTime);
-					$scope.setOlderLink(botTime);
+					$scope.setNewerLink(topTime, sortedData);
+					$scope.setOlderLink(botTime, sortedData);
 				}
 				else {
 					$scope.noArticles="There are no blog entries on this date range";
@@ -66,15 +78,17 @@
 		};
 		$scope.getArticlesNewer = function(posttime) {
 			var oldestPostTime = parseFloat(posttime);
-			$http.get('https://api.schemagames.com/article?limit='+$scope.articlesPerPage+'&newer='+oldestPostTime)
+			$http.get('db/articles.json')
 			.success(function (data) {
-				$scope.articleslist = data;
+				let sortedData = data.sort((a, b) => b.post_time - a.post_time);
+				let takenData = $scope.takeWhile((x) => x.post_time >= oldestPostTime, sortedData).slice(0, $scope.articlesPerPage);
+				$scope.articleslist = takenData;
 				//Set pagination
 				if($scope.articleslist.length > 0) {
 					var topTime = parseFloat($scope.articleslist[0].post_time) + 1;
 					var botTime = parseFloat($scope.articleslist[$scope.articleslist.length - 1].post_time) - 1;
-					$scope.setNewerLink(topTime);
-					$scope.setOlderLink(botTime);
+					$scope.setNewerLink(topTime, sortedData);
+					$scope.setOlderLink(botTime, sortedData);
 				}
 				else {
 					$scope.noArticles="There are no blog entries on this date range";
@@ -84,37 +98,19 @@
 				$scope.articleslist = null;
 			});
 		};
-		$scope.setOlderLink = function(posttime) {
-			$http.get('https://api.schemagames.com/article?older='+posttime)
-				.success(function (data) {
-					var articlecount;
-					if(data.length !== 0) {
-						articlecount = data.length;
-					}
-					else {
-						articlecount = 0;
-					}
-					$scope.oldertime = (articlecount > 0) ?  posttime : null;
-				})
-				.error(function () {
-					$scope.oldertime = null;
-				});
+		$scope.setOlderLink = function(posttime, sortedData) {
+			if(sortedData.some(data => data.post_time <= posttime)) {
+				$scope.oldertime = posttime;
+			} else {
+				$scope.oldertime = null;
+			}
 		};
-		$scope.setNewerLink = function(posttime) {
-			$http.get('https://api.schemagames.com/article?newer='+posttime)
-				.success(function (data) {
-					var articlecount;
-					if(data.length !== 0) {
-						articlecount = data.length;
-					}
-					else {
-						articlecount = 0;
-					}
-					$scope.newertime = (articlecount > 0) ?  posttime : null;
-				})
-				.error(function () {
-					$scope.newertime = null;
-				});
+		$scope.setNewerLink = function(posttime, sortedData) {
+			if(sortedData.some(data => data.post_time >= posttime)) {
+				$scope.newertime = posttime;
+			} else {
+				$scope.newertime = null;
+			}
 		};
 	
 		//Fetch articles
